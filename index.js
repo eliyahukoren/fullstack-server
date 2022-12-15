20ca2e72bf823b746f2603cbac1d87268382503c
eliyahukoren <elik.koren@gmail.com>
2022-12-15 14:36:49 +0200

    install and use mongodb

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("./services/passport");
+const keys = require("./config/keys");
+
+mongoose.set("strictQuery", false);
+mongoose.connect(keys.mongoURI)
+.then( res => {
+	console.log('Connected to MongoDB');
+})
 
 const app = express();
 
