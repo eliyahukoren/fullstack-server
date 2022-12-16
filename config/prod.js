2c4c81c36bbe456d0941858a0747de708f871c37
eliyahukoren <elik.koren@gmail.com>
2022-12-16 20:45:40 +0200

    edit config

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// config/prod.js - production config file
+
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY,
+};
