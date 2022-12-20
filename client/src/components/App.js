f6f6cf4f93a4590ccd7a708b876a29ab0f7f4258
eliyahukoren <elik.koren@gmail.com>
2022-12-20 14:17:58 +0200

    routes and test components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,29 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// import { useDispatch} from "react-redux";
+// import { increment } from "../features/authSlice";
+// import { simplement } from "../features/anySlice";
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+const PageNotFound = () => <h2>PageNotFound</h2>
+
 const App = () => {
-	return <div>Hi There!</div>;
+	// const dispatch = useDispatch();
+
+	return (
+		<div>
+			<BrowserRouter>
+				<Switch>
+					<Route path="/" exact component={Landing} />
+					<Route path="/surveys" exact={true} component={Dashboard} />
+					<Route component={PageNotFound} />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { Provider } from "react-redux";
+import { applyMiddleware} from "redux";
+import store from "./store";
+
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
