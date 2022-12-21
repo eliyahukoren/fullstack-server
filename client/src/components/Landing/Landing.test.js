d28fcb3e40233598ffcd87637191beca9053fdd2
eliyahukoren <elik.koren@gmail.com>
2022-12-21 12:05:20 +0200

    client: add landing page component. first approach

---
+++ client/src/components/Landing/Landing.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+
+describe("Test Landing Component", () => {
+	// TODO : Replace with real test
+	test("mock test", () => {
+		expect(true).toBe(true);
+	});
+});
