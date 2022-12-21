c5d69cead3cbaafbf2845e11a0d8e9aec3b8f5f3
eliyahukoren <elik.koren@gmail.com>
2022-12-21 11:56:21 +0200

    client: authReducer disable warning and add fetch user case

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,12 @@
-export default function (state = {}, action) {
-	console.log(action);
+import { FETCH_USER } from "../actions/types";
+
+// disable warning: Unexpected default export of anonymous function
+// eslint-disable-next-line
+export default function (state = null, action) {
+
 	switch (action.type) {
+		case FETCH_USER:
+			return action.payload || false;
 		default:
 			return state;
 	}
