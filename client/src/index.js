5599123c59042a80a5075e31fd6ba2c16583f48e
eliyahukoren <elik.koren@gmail.com>
2022-12-21 08:47:03 +0200

    client: store, reducer, middleware

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { Provider } from "react-redux";
-import { applyMiddleware} from "redux";
-import store from "./store";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
+import App from "./components/App/App";
+import reducers from "./reducers";
 import "materialize-css/dist/css/materialize.min.css";
 
-import App from "./components/App";
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
